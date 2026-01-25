fcd205f1a01e348c0852bec592ea52dfa8d63dfa
Dominick Albano <domalbano35@gmail.com>
2026-01-25 12:43:55 -0500

    Fix app-shell import casing to match actual filenames

--- src/components/app-shell/index.ts
+++ src/components/app-shell/index.ts
@@ -3,7 +3,7 @@
  * Navigation shell for authenticated app routes
  */
 
-export { Sidebar } from "./sidebar";
-export { Topbar } from "./topbar";
+export { Sidebar } from "./Sidebar";
+export { Topbar } from "./Topbar";
 export { AppShellClient } from "./app-shell-client";
 export { PageHeader } from "./page-header";
